3ed623969adeae3237592b6a4fe9bb1dc54972b0
kamal699-del <kludin707@gmail.com>
2026-09-03 19:18:21 +0700

    Add files via upload

---
+++ layout.js
@@ -0,0 +1,3 @@
+import './globals.css';
+export const metadata={title:'RCM – Management Stock',description:'Sistem manajemen stok LC Rancamanyar'};
+export default function RootLayout({children}){return <html lang="id"><body>{children}</body></html>}
