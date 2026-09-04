413339a75c80a753813648d2be91fe72e2f2e784
kamal699-del <kludin707@gmail.com>
2026-09-04 15:05:00 +0700

    Delete app directory

--- app/layout.js
+++
@@ -1,3 +0,0 @@
-import './globals.css';
-export const metadata={title:'RCM – Management Stock',description:'Sistem manajemen stok LC Rancamanyar'};
-export default function RootLayout({children}){return <html lang="id"><body>{children}</body></html>}
